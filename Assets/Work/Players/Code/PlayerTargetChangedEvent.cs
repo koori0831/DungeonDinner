0588e116983d951551316a4fef5a41960622de7b
Gwamegi <minsu080618@gmail.com>
2026-06-23 00:30:34 +0900

    Reapply "feat : CombatTest"
    This reverts commit c9cfe6d0da3261f4c2675a87431d7bef7257bdb1.

--- Assets/Work/Players/Code/PlayerTargetChangedEvent.cs
+++ Assets/Work/Players/Code/PlayerTargetChangedEvent.cs
@@ -9,4 +9,4 @@ namespace Work.Players.Code
     /// <param name="Target">변경된 플레이어 Transform</param>
     /// <param name="IsRegistered">등록 여부</param>
     public readonly record struct PlayerTargetChangedEvent(Transform Target, bool IsRegistered) : IEvent;
-}
+}    
