2bbda5cc260173cfe23e91576fe23471884fab05
Gwamegi <minsu080618@gmail.com>
2026-07-08 19:21:02 +0900

    fix : UIlayout

--- Assets/Work/Adventure/Code/UI/AdventureDialogUI.cs
+++ Assets/Work/Adventure/Code/UI/AdventureDialogUI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Rendering.MaterialUpgrader;
 
 namespace Work.Adventure.Code.UI
 {
